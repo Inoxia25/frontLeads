574b7169abc71f825d09732f08fc2c4e5a7d96ff
Swatilekha Roy <swatilekha.roy4@gmial.com>
2021-05-08 21:41:14 +0530

    added first card

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,15 +5,27 @@ import { HttpClientModule } from '@angular/common/http';
 import { HttpClient } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LeadCardComponent } from './lead-card/lead-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeadCardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
