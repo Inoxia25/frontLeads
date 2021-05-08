bd5b2c0b1a9b2de608b1298403105180dbe2fdca
Swatilekha Roy <swatilekha.roy4@gmial.com>
2021-05-09 02:15:58 +0530

    Added form component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClient } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
@@ -12,22 +11,34 @@ import { MatCardModule } from '@angular/material/card';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LeadCardComponent } from './lead-card/lead-card.component';
 import { CoolSocialLoginButtonsModule } from '@angular-cool/social-login-buttons';
+import { LeadFormComponent } from './lead-form/lead-form.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule} from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatRadioModule } from '@angular/material/radio';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LeadCardComponent
+    LeadCardComponent,
+    LeadFormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
     BrowserAnimationsModule,
     MatIconModule,
     MatButtonModule,
     MatToolbarModule,
     MatCardModule,
     CoolSocialLoginButtonsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatRadioModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
