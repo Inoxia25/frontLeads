3a05e3fb820cf8e4a58b9c27048ef02dfee9a3af
Swatilekha Roy <swatilekha.roy4@gmail.com>
2021-05-09 16:56:13 +0530

    Add files via upload

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,52 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LeadCardComponent } from './lead-card/lead-card.component';
+import { CoolSocialLoginButtonsModule } from '@angular-cool/social-login-buttons';
+import { LeadFormComponent } from './lead-form/lead-form.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule} from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatRadioModule } from '@angular/material/radio';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import { FilterComponent } from './filter/filter.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LeadCardComponent,
+    LeadFormComponent,
+    FilterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatCardModule,
+    CoolSocialLoginButtonsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatRadioModule,
+    MatSnackBarModule,
+    MatSidenavModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
